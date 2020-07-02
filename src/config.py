f29ad23d5a6df25d6413b3a9d837374a04e740a3
Fernando <fernaperg@gmail.com>
2020-07-02 11:24:15 +0200

    :wrench: Adapted optional configs

--- src/config.py
+++ src/config.py
@@ -1,13 +1,14 @@
 SEND_EMAIL = False
 
-COMPANY = ''
-DNS = ''
+if SEND_EMAIL:
+    COMPANY = ''
+    DNS = ''
 
-########### PYTHON SMTP CONFIG ##############
-ALERT_RECEIVERS = [""] # dif <---
-ALERT_SENDER = "" # dif <---
-SMTP_HOST = "smtp.gmail.com"
-SMTP_PORT = 587
-SMTP_USER = "" # dif <---
-SMTP_PASS = "" # dif <---
-#############################################
+    ########### PYTHON SMTP CONFIG ##############
+    ALERT_RECEIVERS = [""] # dif <---
+    ALERT_SENDER = "" # dif <---
+    SMTP_HOST = "smtp.gmail.com"
+    SMTP_PORT = 587
+    SMTP_USER = "" # dif <---
+    SMTP_PASS = "" # dif <---
+    #############################################
